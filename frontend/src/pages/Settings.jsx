027fe512ba906133b004eb4cefabae0279d03898
vidhirawat10 <vidhirawat54@gmail.com>
2026-02-19 20:10:10 +0530

    delete functionality

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "../../api/axios"; // adjust if path different
+import axios from "../api/axios";
 import "../styles/settings.css";
 
 const Settings = () => {
