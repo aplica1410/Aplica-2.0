de1a629dfc27655ebd5f1e7b050e8a990eda831e
vidhirawat10 <vidhirawat54@gmail.com>
2026-02-26 22:58:01 +0530

    logo adding

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Link } from "react-router-dom";
-import logo from "../assets/logo.svg";
+import logo from "../../assets/logo.svg";
 import "./sidebar.css";
 
 const Sidebar = () => {
